6568b5a11ab367b4d03f7c2ab3c3de7130437760
CozyDoomer <chr.unterrainer@gmail.com>
2020-02-16 23:06:39 +0100

    finish polybar modules and design
    add picom conf for opacity
    
    restructure repo

--- .config/polybar/i3wmthemer_bar_launch.sh
+++ .config/polybar/i3wmthemer_bar_launch.sh
@@ -7,12 +7,9 @@ sleep 0.2;
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     if [ $m = "DVI-D-0" ]; then
-      export TRAY_POS=right
+      MONITOR=$m polybar --reload i3wmthemer_bar_main &
     else
-      unset TRAY_POS
+      MONITOR=$m polybar --reload i3wmthemer_bar_secondary &
     fi
-    MONITOR=$m polybar --reload i3wmthemer_bar &
   done
-else
-  polybar --reload i3wmthemer_bar &
 fi

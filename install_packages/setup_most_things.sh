7fdc079525976a7cf07b0c53ed8be5b4137b7fb0
cozyDoomer <chr.unterrainer@gmail.com>
2020-09-26 06:01:19 +0200

    add rounded i3 borders and alternating layouts on 3+ windows

--- install_packages/setup_most_things.sh
+++ install_packages/setup_most_things.sh
@@ -3,9 +3,12 @@ sudo pacman -Syu
 
 # install packages from pkg-list.txt
 sudo pacman -S --needed - < pkg-list.txt
+
+# build aur packages
 pamac build aws-cdk
 pamac build anydesk-bin
 pamac build teams
+pamac build i3-gaps-rounded-git
 
 # start docker service, add it to autostart create docker group and add user
 systemctl start docker.service

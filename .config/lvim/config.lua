6e52e91a1389504136f898dd7128ddfe35a57317
cozyDoomer <chr.unterrainer@gmail.com>
2022-06-01 17:18:06 +0200

    change telescope keybinds in lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -157,6 +157,15 @@ linters.setup {
   },
 }
 
+lvim.builtin.telescope.on_config_done = function()
+  local actions = require "telescope.actions"
+  lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
+  lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
+  lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
+  lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
+  lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+end
+
 -- Additional Plugins
 lvim.plugins = {
     {"lunarvim/colorschemes"},
